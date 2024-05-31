7b5cd4d8c46d937efa8a348a817f9740263c3717
Dev 01 <dev01@Devs-MacBook-Pro.local>
2024-06-01 02:16:48 +0600

    open credite system

--- onlineJudge/openCreditSystem.cpp
+++ onlineJudge/openCreditSystem.cpp
@@ -1,18 +1,58 @@
-#include <iostream>
+// #include <iostream>
+// #include <limits.h>
+// using namespace std;
+
+
+
+// int main () {
+//     int student[200];
+//     int numberOfTestCase;
+//     cin >> numberOfTestCase;
+//     while (numberOfTestCase--) {
+//         int numberOfInput;
+//         cin >> numberOfInput;
+//         int maxDif = 0;
+//         int max = 0;
+//         for (int i = 0; i < numberOfInput; i++) {
+//             scanf("%d ", &student[i]);
+//         }
+
+//         for (int  i = 0; i < numberOfInput - 1 ; i++)
+//         {
+//          if (student[i] > max  ) {
+//             max = student[i];
+//          }
+
+//          if (max - student[i + 1] > maxDif) {
+//             maxDif = max - student[i + 1];
+//          }
+
+//         }
+//         cout << maxDif << endl;
+        
+//     }
+//     return 0;
+// }
+
+#include<cstdio>
+ 
 using namespace std;
-
-
-
-int main () {
-    int numberOfTestCase;
-    cin >> numberOfTestCase;
-    while (numberOfTestCase--) {
-        int numberOfInput;
-        cin >> numberOfInput;
-        while(numberOfInput--) {
-            int n;
-            cin >> n;
-        }
+ 
+int T, n, s[200000];
+ 
+int main() {
+    scanf("%d", &T);
+    while(T--) {
+        scanf("%d", &n);
+        int max = -200000; int max_dif = -200000;
+        for(int i = 0; i < n; i++)
+            scanf("%d", &s[i]);
+        for(int i = 0; i < n - 1; i++) {
+            if(s[i] > max)
+                max = s[i];
+            if(max - s[i + 1] > max_dif)
+                max_dif = max - s[i + 1];
+        } 
+        printf("%d\n", max_dif);
     }
-    return 0;
 }
